09908996329c7f9930f3cb8934022a66e457db90
zhenling_mo@qq.com <zhenling_mo@qq.com>
2024-09-20 20:04:55 +0800

    update date

--- git_update.sh
+++ git_update.sh
@@ -2,7 +2,7 @@
 
 # Update the date in the file
 FILE="./_pages/about.md"
-DATE=$(date +"%d %b %Y")
+DATE=$(LC_TIME="en_US.UTF-8" date +"%d %b %Y")
 sed -i "s/last_modified_at: .*/last_modified_at: $DATE/" $FILE
 
 echo "Updated 'last_modified_at' to $DATE in $FILE locally"
